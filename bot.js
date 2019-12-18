71fbf5b5c44a78a8124d013d2a0be676c58e7faf
Kayk <ksm.kayk@outlook.com>
2019-12-18 16:42:57 -0300

    Configurações basicas para manisfestação do bot e adicionada dotenv

--- bot.js
+++ bot.js
@@ -1,4 +1,30 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 const config = require("./config.json");
-const env = require("./.env");
+require("dotenv/config");
+
+client.on("ready", () => {
+  console.log(
+    `Bot foi iniciado, com ${client.users.size} usuarios, 
+    em ${client.channels.size} canais, 
+    em ${client.guilds.size} servidores`
+  );
+  client.user.setGame(`Estou em ${client.guilds.size} servidores`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(
+    `O bot entrou no servidor: ${guild.name} (id: ${guild.id}).
+     População ${guild.memberCount} membros`
+  );
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`O bot foi removido do servidor: ${guild.name}`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+client.on("message", async message => {});
+
+client.login(process.env.token);
